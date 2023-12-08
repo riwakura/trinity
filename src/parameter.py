4af00c7e6b7cf09ed7d2f7077517089efdaf9fda
riwakura <232s404s@stu.kobe-u.ac.jp>
2023-12-08 15:28:19 +0900

    added class Prograde, Retrograde, XY

--- src/parameter.py
+++ src/parameter.py
@@ -4,17 +4,27 @@ import numpy as np
 
 PI = math.acos(-1)
 
+class XY:
+    def __init__(self, _mass, _x, _y, _z, _vx, _vy, _vz):
+        self.mass = _mass
+        self.x = _x
+        self.y = _y
+        self.z = _z
+        self.vx = _vx
+        self.vy = _vy
+        self.vz = _vz
+
 class Prograde:
-    def __init__(self,time,semiin,semiout,eccin,eccout,Omgin,Omgout,omgin,omgout):
-        self.time = time
-        self.semiin = semiin
-        self.semiout = semiout
-        self.eccin = eccin
-        self.eccout = eccout
-        self.Omgin = Omgin
-        self.Omgout = Omgout
-        self.omgin = omgin
-        self.omgout = omgout
+    def __init__(self,_time,_semiin,_semiout,_eccin,_eccout,_rot_horin,_rot_horout,_omgin,_omgout):
+        self.time = _time
+        self.semiin = _semiin
+        self.semiout = _semiout
+        self.eccin = _eccin
+        self.eccout = _eccout
+        self.rot_horin = _rot_horin
+        self.rot_horout = _rot_horout
+        self.omgin = _omgin
+        self.omgout = _omgout
     
     def semiratio(self):
         return self.semiin / self.semiout
@@ -23,10 +33,10 @@ class Prograde:
         return self.semiout(1-self.eccout) / self.semiin
     
     def netomgin(self):
-        if math.cos(self.Omgin + self.omgin) > 0:
-            return math.atan2(self.Omgin+self.omgin , 1)
-        if math.cos(self.Omgin + self.omgin) < 0:
-            return math.atan2(self.Omgin+self.omgin , -1)
+        if math.cos(self.rot_horin + self.omgin) > 0:
+            return math.atan2(self.rot_horin+self.omgin , 1)
+        if math.cos(self.rot_horin + self.omgin) < 0:
+            return math.atan2(self.rot_horin+self.omgin , -1)
 
     def netomgout(self):
         if math.cos(self.Omgout + self.omgout) > 0:
@@ -41,16 +51,16 @@ class Prograde:
             return Prograde.netomgin - Prograde.netomgout + 2*PI
         
 class Retrograde:
-    def __init__(self,time,semiin,semiout,eccin,eccout,Omgin,Omgout,omgin,omgout):
-        self.time = time
-        self.semiin = semiin
-        self.semiout = semiout
-        self.eccin = eccin
-        self.eccout = eccout
-        self.Omgin = Omgin
-        self.Omgout = Omgout
-        self.omgin = omgin
-        self.omgout = omgout
+    def __init__(self,_time,_semiin,_semiout,_eccin,_eccout,_rot_horin,_rot_horout,_omgin,_omgout):
+        self.time = _time
+        self.semiin = _semiin
+        self.semiout = _semiout
+        self.eccin = _eccin
+        self.eccout = _eccout
+        self.rot_horin = _rot_horin
+        self.rot_horout = _rot_horout
+        self.omgin = _omgin
+        self.omgout = _omgout
     
     def semiratio(self):
         return self.semiin / self.semiout
@@ -60,15 +70,15 @@ class Retrograde:
     
     def netomgin(self):
         if math.cos(self.Omgin - self.omgin) > 0:
-            return math.atan2(self.Omgin-self.omgin , 1)
+            return math.atan2(self.rot_horout-self.omgin , 1)
         if math.cos(self.Omgin - self.omgin) < 0:
-            return math.atan2(self.Omgin-self.omgin , -1)
+            return math.atan2(self.rot_horout-self.omgin , -1)
 
     def netomgout(self):
-        if math.cos(self.Omgout - self.omgout) > 0:
-            return math.atan2(self.Omgout-self.omgout, 1)
-        if math.cos(self.Omgout - self.omgout) < 0:
-            return math.atan2(self.Omgout-self.omgout ,-1)
+        if math.cos(self.rot_horout - self.omgout) > 0:
+            return math.atan2(self.rot_horout-self.omgout, 1)
+        if math.cos(self.rot_horout - self.omgout) < 0:
+            return math.atan2(self.rot_horout-self.omgout ,-1)
         
     def omgdiff(self):
         if Prograde.netomgin - Prograde.netomgout > 0:
